926fe3101ac76cf37333d421ca170a21e4d2c045
Karempudi <praneeth.karempudi@gmail.com>
2022-08-15 12:18:19 +0200

    Added functions for root logger

--- sting/utils/logger.py
+++ sting/utils/logger.py
@@ -1,4 +1,42 @@
 import logging
+import logging.handlers
+import torch.multiprocessing as tmp
+from .types import RecursiveNamespace
 
-def setup_logger():
-    pass
+def setup_root_logger(param: RecursiveNamespace):
+    """
+    
+    Setting up the properties of the root logger of 
+    an experiment
+
+    Args:
+        param (RecursiveNamespace) : Namespace of the parameters
+            used for the experiment
+    """
+    root = logging.getLogger()
+    f = logging.Formatter('%(asctime)s %(processName)-10s %(levelname)-8s %(messages)s')
+    if param.Logging.to_file:
+        h = logging.FileHandler(filename=param.Logging.directory, mode='w')
+        h.setFormatter(f)
+        root.addHandler(h)
+
+    if param.Logging.to_console:
+        c = logging.StreamHandler()
+        c.setFormatter(f)
+        root.addHandler(c)
+
+def logger_listener(queue, param):
+    setup_root_logger(param)
+    while True:
+        try:
+            record = queue.get()
+            if record is None:
+                break
+            
+            logger = logging.getLogger(record.name)
+            logger.handle(record)
+        except Exception:
+            import sys, traceback
+            print('Whoops! Logger has a problem: ', file=sys.stderr)
+            traceback.print_exec(file=sys.stderr)
+    
