f58c5dfce17a57400257b48a2c3f034589bd142b
Praneeth Karempudi <praneeth.karempudi@gmail.com>
2022-08-18 16:22:24 +0200

    Working on training code of segmentation network, added argument parsing and basic config layout

--- sting/segmentation/train.py
+++ sting/segmentation/train.py
@@ -1,14 +1,55 @@
 import argparse
+from pathlib import Path
+
+import torch
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Segmentation networks Training Arguments")
 
+    parser.add_argument('-p', '--param_file', 
+            help='Specify the param file for training segmentation network.',
+            required=True)
+    
+    parser.add_argument('-d', '--device',
+            help='Specify the device string (cpu, cuda, cuda:0 or cuda:1) and overwrite parameters.',
+            type=str, required=False)
+    
+    parser.add_argument('-w', '--num_workers_override',
+            help='Override number of workers of pytorch dataloaders.', type=int)
+    
+    parser.add_argument('-l', '--log_dir', defaults='segment_runs',
+            help='Set directory name where you want the logs to go in.')
+    
+    parser.add_argument('-c', '--log_comment', default=None,
+            help='Added a log comment to the run')
+
     parser.add_argument()
+
+    return args
+
+def train_model(param_file: str, device_overwrite: str = None,
+            num_workers_overwrite: int = None,
+            log_dir: str = 'segment_runs', log_comment: str = None):
+    """
+    Train a segmentation model based on the parameters given
+
+    Args:
+        param_file (str): parameter file path, includes everything need to 
+                train a segmentation network, U-net, Omnipose and their 
+                variants for both cell and channel detection
+        device_overwrite (str): overwrite cuda device specified in param_file
+        num_workers_overwrite (int): overwrite num of workers in pytorch dataloaders
+        log_dir (str): directory for logs used by tensorboard
+        log_comment (str): comment to the experiment
+
+    """
+    pass
 
 def main():
     print("Hello from segmentation training")
     args = parse_args()
 
+    train_model()
 
 if __name__ == "__main__":
     main()
