df550964da5de27647d80b15250e3b1dd9e5cb38
Praneeth Karempudi <praneeth.karempudi@gmail.com>
2022-09-22 15:39:13 +0200

    Added dataset collation function for dataloading regiondetection dataset

--- sting/regiondetect/datasets.py
+++ sting/regiondetect/datasets.py
@@ -3,6 +3,7 @@ from tifffile import imread
 import pathlib
 from pathlib import Path
 import numpy as np
+import torch
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from torch.utils.data import Dataset
@@ -18,7 +19,7 @@ class BarcodeDataset(Dataset):
         data_dir (pathlib.Path or str): path of the data directory 
     
     """
-    def __init__(self, data_dir, img_size=(256, 800), multiscale=True, transform=None):
+    def __init__(self, data_dir, img_size=(256, 800), multiscale=False, transform=None):
         super(BarcodeDataset, self).__init__()
         self.data_dir = Path(data_dir) if isinstance(data_dir, str) else data_dir
         assert isinstance(data_dir, pathlib.Path) == False, "data directory is not a valid path"
@@ -38,6 +39,9 @@ class BarcodeDataset(Dataset):
         self.data_tuples = [(self.images_dir / Path(filestem + '.tiff'),
                              self.labels_dir / Path(filestem + '.txt')) 
                                     for filestem in filestems]
+
+        # not figured out where this will be used exactly
+        self.batch_count = 0
         
     def __len__(self):
         return len(self.data_tuples)
@@ -62,14 +66,40 @@ class BarcodeDataset(Dataset):
             bboxes = transformed['bboxes']
         return {
             'image': image,
-            'bboxes': bboxes
+            'bboxes': bboxes,
+            'path': str(image_filename)
         }
+
+    def collate_fn(self, batch):
+        self.batch_count += 1 
+
+        # drop invalid images
+        batch = [data for data in batch if data is not None]
+
+        # images stack
+        images = torch.stack([data['image'] for data in batch])
+
+        # you need to impelement multiscale option here
+        # TODO: later. Need to figure out how bboxes need
+        # to be transformed for multiscale training
+        # for now, we do only one scale
+
+        bb_targets = [data['bboxes'] for data in batch]
+        for i, bboxes in enumerate(bb_targets):
+            # first col of each bbox in a batch corresponds to image idx in the batch
+            bboxes[:, 0] = i
+        bb_targets = torch.cat(bb_targets, 0)
+
+        paths = [data['path'] for data in batch]
+        
+        return paths, images, bb_targets
     
     def plot_datapoint(self, idx):
         datapoint = self.__getitem__(idx)
         image = datapoint['image']
         #print(image.shape)
         bboxes = datapoint['bboxes']
+        #print(bboxes)
         if image.ndim == 3:
             image = image[0, :,:,]
         #image = image/65535
