281d6e311a07e250394b24242f3ac90a550d45d6
Praneeth Karempudi <praneeth.karempudi@gmail.com>
2022-11-07 16:20:42 +0100

    Modified tiny bits in writing files

--- sting/utils/disk_ops.py
+++ sting/utils/disk_ops.py
@@ -34,28 +34,36 @@ def write_files(event_data, event_type, param):
         if not position_dir.exists():
             position_dir.mkdir(exist_ok=True, parents=True)
         
-        # create event directory
-        events_dir = position_dir / Path(event_type)
-        if not events_dir.exists():
-            events_dir.mkdir(exist_ok=True, parents=True)
         
         if event_type == 'phase':
+            # create event directory
+            events_dir = position_dir / Path(event_type)
+            if not events_dir.exists():
+                events_dir.mkdir(exist_ok=True, parents=True)
             # put the image in phase directory at position with timepoint 
-            image_filename = str(event_data['time']) + '.tiff'
+            image_filename = 'phase_' + str(event_data['time']).zfill(4) + '.tiff'
             image_filename = events_dir / Path(image_filename)
             imsave(image_filename, event_data['image'].astype('uint16'))
         elif event_type == 'cells_channels':
-            #
+            # create event directory
+            cells_events_dir = position_dir / Path('segmented_cells')
+            if not cells_events_dir.exists():
+                cells_events_dir.mkdir(exist_ok=True, parents=True)
+
+            channels_events_dir = position_dir / Path('segmented_channels')            
+            if not channels_events_dir.exists():
+                channels_events_dir.mkdir(exist_ok=True, parents=True)
+
             cells_filename = 'cells_' + str(event_data['time']).zfill(4) + '.tiff'
-            cells_filename = events_dir / Path(cells_filename)
+            cells_filename = cells_events_dir / Path(cells_filename)
             channels_filename = 'channels_' + str(event_data['time']).zfill(4) + '.tiff'
-            channels_filename = events_dir / Path(channels_filename)
+            channels_filename = channels_events_dir / Path(channels_filename)
             cell_prob = param.Analysis.Segmentation.thresholds.cells.probability
             channel_prob = param.Analysis.Segmentation.thresholds.channels.probability
             imsave(cells_filename, (event_data['cells'] > cell_prob).astype('float32'),
-                    plugin='tifffile', check_contrast=False)
+                    plugin='tifffile', check_contrast=False, compress=6)
             imsave(channels_filename, (event_data['channels'] > channel_prob).astype('float32'), 
-                    plugin='tifffile', check_contrast=False)
+                    plugin='tifffile', check_contrast=False, compress=6)
         
     except KeyError:
         sys.stdout.write(f"Writing failed for due to lack of position key in data ..\n")
