a9a852219056853cb44f1b7d541653ba8a0bd884
Praneeth Karempudi <praneeth.karempudi@gmail.com>
2022-12-27 16:22:55 +0100

    Small bug fix in the event generation loop

--- sting/microscope/acquisition.py
+++ sting/microscope/acquisition.py
@@ -6,6 +6,7 @@ from sting.utils.types import RecursiveNamespace
 from itertools import cycle, repeat
 from sting.microscope.motion import MotionFromFile, RectGridMotion, TwoRectMotion 
 from tifffile import imread
+import numpy as np
 
 class ExptAcquisition(object):
     """
@@ -55,7 +56,7 @@ class ExptAcquisition(object):
         elif self.rules.units == 'minutes':
             self.time_factor = 60
 
-        self.min_loop_start_times = list(range(self.rules.start, self.rules.end, self.rules.every))
+        self.min_loop_start_times = list(np.arange(self.rules.start, self.rules.end, self.rules.every))
         self.n_loops = len(self.min_loop_start_times)
         self.loop_interval = self.rules.every * self.time_factor
 
@@ -63,13 +64,13 @@ class ExptAcquisition(object):
             for i, position_data in enumerate(self.positions, 0):
                 event = {}
                 # grab position number from the 
-                event['axes']  = {'time': loop_no, 'position': int(position_data['label'][3:])}
+                event['axes']  = {'time': loop_no+1, 'position': int(position_data['label'][3:])}
                 event['x'] = position_data['x']
                 event['y'] = position_data['y']
                 event['z'] = position_data['z']
                 event['channel'] = {'group': self.rules.group, 'config': self.rules.preset}
                 event['exposure'] = self.rules.exposure 
-                event['min_start_time'] = self.min_loop_start_times[loop_no] * self.loop_interval
+                event['min_start_time'] = self.min_loop_start_times[loop_no] * self.time_factor
                 self.events.append(event)
             if self.rules.slow_positions == 'first':
                 self.events[0]['channel']['config'] = self.rules.slow_preset
